0034390f7aa39e65786553abbec14c7501243164
Avanish jha <avnishjha277@gmil.com>
2025-06-02 19:15:21 +0530

    Update site build 2025-06-02 19:15:21

--- AS_Tech16/deploy.sh
+++ AS_Tech16/deploy.sh
@@ -3,17 +3,14 @@
 
 set -e
 
-BUILD_DIR="../build"
+SOURCE_DIR="../composeApp/build/dist/wasmJs/productionExecutable"
 TARGET_DIR="."
 
-# Clean old build files (excluding .git and this script)
 shopt -s extglob
 rm -rf !(.git|deploy.sh)
 
-# Copy fresh build output
-cp -r "$BUILD_DIR"/* "$TARGET_DIR"/
+cp -r "$SOURCE_DIR"/* "$TARGET_DIR"/
 
-# Commit and push
 git add .
 git commit -m "Update site build $(date '+%Y-%m-%d %H:%M:%S')" || echo "No changes to commit"
 git push origin main
